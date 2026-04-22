1555df7c7ea7e383d760ba117371e3262b14fb7b
natalia <dabkowicznatalia@gmail.com>
2026-04-22 19:02:55 +0200

    added fish and vector2d method - two base methods that will be used by most other classes

--- src/main/java/com/logic/game/player/Fish.java
+++ src/main/java/com/logic/game/player/Fish.java
@@ -1,4 +1,31 @@
-package main.java.com.logic.game.player;
+package com.logic.game.player;
+import com.logic.game.utils.Vector2D;
 
 public class Fish {
+    private Vector2D position; // pozycja
+    private int width = 50;    // jak szeroka jest (do kolizji)
+    private int height = 30;   // jak wysoka jest
+
+    public Fish(float startX, float startY) {
+        //pozycja startowa
+        this.position = new Vector2D(startX, startY);
+    }
+
+    //sterowanie ryba
+    public void move(float tiltX, float tiltY) {
+        //tiltX przechylenie telefonu na boki
+        position.x += tiltX * 5;
+
+        //tiltY to pochylenie przod/tyl - wplywa na predkosc y
+        position.y += tiltY * 2;
+
+        //zabezpieczenie: ryba nie może wyplynac poza ekran zalozona szereokosc 500
+        if (position.x < 0) position.x = 0;
+        if (position.x > 450) position.x = 450;
+    }
+
+    // getter pozwalajacy innym klasom na sprawdzenie polozenia ryby
+    public Vector2D getPosition() {
+        return position;
+    }
 }
--- src/main/java/com/logic/game/utils/Vector2D.java
+++ src/main/java/com/logic/game/utils/Vector2D.java
@@ -1,4 +1,12 @@
-package main.java.com.logic.game.utils;
+package com.logic.game.utils;
 
 public class Vector2D {
+    public float y;    //pozycja lewo prawo
+    public float x;    //gora dol
+
+
+    public Vector2D(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
 }
