ec09414a69dfe6a6e8f3e96d1fbb537bf7eb65ab
natalia <dabkowicznatalia@gmail.com>
2026-04-22 19:51:38 +0200

    wrote a basic fish physics, included velocity and friction of water

--- src/main/java/com/logic/game/player/PlayerPhysics.java
+++ src/main/java/com/logic/game/player/PlayerPhysics.java
@@ -1,4 +1,27 @@
 package com.logic.game.player;
 
+import com.logic.game.utils.Vector2D;
+
 public class PlayerPhysics {
+
+    private final float ACCELERATION_SENSIVITY = 0.5f;
+    private final float FRICTION = 0.9f;    //OPOR WODY SPOWALNIAJACY RYBE gdy nie jest sterowana
+    private final float MAX_VELOCITY = 15.0f;
+
+    private float velocityX = 0;
+    private float velocityY = 0;
+
+    //do obliczenia nwoej predkosci na podst danych z zyroskopu
+
+    public float calculateNewVelocityX(float tiltX) {
+        velocityX += tiltX * ACCELERATION_SENSIVITY;
+        velocityX *= FRICTION;  //nalozony opor aby zapobiec slizaganiu sie w nieskonczonosc ryby
+
+        //OGR MAX V
+        if (velocityX > MAX_VELOCITY) velocityX = MAX_VELOCITY;
+        if (velocityX < -MAX_VELOCITY) velocityX = -MAX_VELOCITY;
+
+        return velocityX;
+        }
 }
+
